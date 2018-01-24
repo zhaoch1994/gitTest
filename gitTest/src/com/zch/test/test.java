3febe7da75c80bfc01cc579d21b3a819ea1ba57f
zhaochh <zhaochunhui03@meituan.com>
2018-01-24 14:24:45 +0800

    分支第一次提交

--- gitTest/src/com/zch/test/test.java
+++ gitTest/src/com/zch/test/test.java
@@ -3,9 +3,7 @@ package com.zch.test;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("zch 测试");
-	}
-	class sa{
-		
+		System.out.println("这是分支二的代码");
 	}
 	private int i=1;
 
---
+++ gitTest/src/com/zch/test/test1.java
@@ -0,0 +1,5 @@
+package com.zch.test;
+
+public class test1 {
+
+}
