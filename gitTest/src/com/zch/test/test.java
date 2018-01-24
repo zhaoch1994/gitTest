1d1c2a8c49afc3379991f57bf0f5fa6fec1c4d27
zhaochh <zhaochunhui03@meituan.com>
2018-01-24 15:05:14 +0800

    分支二第二次提交代码

--- gitTest/src/com/zch/test/test.java
+++ gitTest/src/com/zch/test/test.java
@@ -3,7 +3,7 @@ package com.zch.test;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("zch 测试");
-		System.out.println("这是分支二的代码");
+		System.out.println("这是分支二第二次提交的代码");
 	}
 	private int i=1;
 
